57abfdfe5136971d3eba9e8d30f407e3e3af60c5
marcosrafaellsousa <marcosrafaellsousa@gmail.com>
2021-07-16 13:59:28 -0300

    secao 7 estrutura de um projeto com Page Object

---
+++ secao 7/tests/features/pages/primeiro_object.rb
@@ -0,0 +1,11 @@
+class Primeiro_object < SitePrism::Page
+
+# mapear os elementos
+# fazer nossos metodos
+
+    # @page = Primeiro_object.new
+
+    # @page.elemento.click
+    # @page.metodo
+
+end
---
+++ secao 7/tests/features/support/env.rb
@@ -0,0 +1,11 @@
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'webdrivers'
+require 'site_prism'
+
+Capybara.configure do |config|
+# Drivers: selenium   selenuim_chrome    selenuim_chrome_headless
+    config.default_driver = :selenium_chrome
+    config.app_host = 'https://getbootstrap.com'
+    config.default_max_wait_time = 5
+end
