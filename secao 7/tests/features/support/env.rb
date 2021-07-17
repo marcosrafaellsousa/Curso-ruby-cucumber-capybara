d2dd610b0fddd20d5c55e667a3cadc4f0a722d35
marcosrafaellsousa <marcosrafaellsousa@gmail.com>
2021-07-17 17:49:16 -0300

    Configurando o selenium em outros navegadores

--- secao 7/tests/features/support/env.rb
+++ secao 7/tests/features/support/env.rb
@@ -6,13 +6,28 @@ require_relative 'page_helper.rb'
 
 World(PageObjects)
 
+BROWSER = ENV['BROWSER']
 AMBIENTE = ENV['AMBIENTE']
 
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
+# Sobrescrevendo o driver do selenium para usar o browser que quisermos
+# Também vale para outros navegadores como safari, poltergeist etc.
+Capybara.register_driver :selenium do |app|
+
+    if BROWSER.eql?('chrome')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome,)
+    elsif BROWSER.eql?('firefox')
+        Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>TRUE)
+    elsif BROWSER.eql?('ie')
+        Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+    end
+end
+
 Capybara.configure do |config|
 # Drivers: selenium   selenuim_chrome    selenuim_chrome_headless
-    config.default_driver = :selenium_chrome
+# Por padrão o driver selenium usa o firefox
+    config.default_driver = :selenium
     config.app_host = CONFIG['url_padrao']
     config.default_max_wait_time = 5
 end
