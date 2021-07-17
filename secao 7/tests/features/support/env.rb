706d3e8995151af0c8eb882fdb6f2b34b3260fe6
marcosrafaellsousa <marcosrafaellsousa@gmail.com>
2021-07-17 15:49:42 -0300

    criando diferentes ambientes para os testes

--- secao 7/tests/features/support/env.rb
+++ secao 7/tests/features/support/env.rb
@@ -6,9 +6,13 @@ require_relative 'page_helper.rb'
 
 World(PageObjects)
 
+AMBIENTE = ENV['AMBIENTE']
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
+
 Capybara.configure do |config|
 # Drivers: selenium   selenuim_chrome    selenuim_chrome_headless
     config.default_driver = :selenium_chrome
-    config.app_host = 'https://getbootstrap.com'
+    config.app_host = CONFIG['url_padrao']
     config.default_max_wait_time = 5
 end
